03dec7e27e8374a054c1805eb348b6fee07483b7
Francesco Occhipinti <f.occhipinti@gmail.com>
2011-12-06 15:03:32 +0100

    cleaned a little

---
+++ setup.py
@@ -0,0 +1,30 @@
+
+from setuptools import setup, find_packages, Command
+import pytest
+
+class PyTest(Command):
+    user_options=[]
+    def initialize_options(self):pass
+    def finalize_options(self):pass
+    def run(self):
+        args = ['--doctest-modules', '-q', 'granpa']
+        pytest.main(args)
+
+setup(
+    name='granpa',
+    scripts=['scripts/granpa.py'],
+    install_requires=[
+        'python-daemon',
+        'pytest',
+        'ini_parse==dev',
+        'meta==dev',
+        'doctest_utils==dev',
+        ],
+    dependency_links=[
+        'https://github.com/danse/ini_parse/raw/master/ini_parse.py#egg=ini_parse-dev',
+        'https://github.com/danse/meta/raw/master/meta.py#egg=meta-dev',
+        'https://github.com/danse/doctest_utils/raw/master/doctest_utils.py#egg=doctest_utils-dev',
+        ],
+    packages=find_packages(),
+    cmdclass={'test':PyTest},
+)
