bef1d28d2a42063b4f121756ed0c915177008e74
Francesco Occhipinti <f.occhipinti@gmail.com>
2011-12-15 19:10:45 +0100

    added entry points and try except

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 
 from setuptools import setup, find_packages, Command
-import pytest
-
-class PyTest(Command):
-    user_options=[]
-    def initialize_options(self):pass
-    def finalize_options(self):pass
-    def run(self):
-        args = ['--doctest-modules', '-q', 'granpa']
-        pytest.main(args)
+try:
+    import pytest
+    class PyTest(Command):
+        user_options=[]
+        def initialize_options(self):pass
+        def finalize_options(self):pass
+        def run(self):
+            args = ['--doctest-modules', '-q', 'granpa']
+            pytest.main(args)
+except ImportError:
+    class PyTest(Command):pass
 
 setup(
     name='granpa',
-    scripts=['scripts/granpa.py'],
+    scripts=['scripts/granpa_info'],
+    entry_points={
+        'console_scripts':[
+            'granpa = granpa.core:main_func',
+            ]
+        },
     install_requires=[
-        'python-daemon',
         'pytest',
+        'python-daemon',
         'ini_parse==dev',
         'meta==dev',
         'doctest_utils==dev',
