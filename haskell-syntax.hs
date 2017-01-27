7405373c6e28eaadbda50fc357335b5b99174e0b
danse <f.occhipinti@gmail.com>
2017-01-27 13:09:47 +0100

    add other snippets to my notes about Haskell syntax

---
+++ haskell-syntax.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings, DeriveDataTypeable #-}
+
+class Functor f => Applicative f where
+  pure  :: a -> f a
+  infixl 4 <*>
+  (<*>) :: f (a -> b) -> f a -> f b
+
+instance Monoid [a] where
+        mempty  = []
+
+f a =
+  let b = a * 2
+  in b + a
+
+guarded something
+  | condition something = result
+  | otherwise           = somethingElse
+
+
+-- in the following case, the indentation provided by `haskell-mode`
+-- will be uglier
+ifMonadic = do
+  somethingMonadic
+  if somethingIf
+    then
+      do
+        somethingThen
+    else
+      do
+        somethingElse
--- haskell.md
+++
@@ -1,7 +0,0 @@
-class Functor f => Applicative f where
-  pure  :: a -> f a
-  infixl 4 <*>
-  (<*>) :: f (a -> b) -> f a -> f b
-
-instance Monoid [a] where
-        mempty  = []
